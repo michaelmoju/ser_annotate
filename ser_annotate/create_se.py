38a885f96eb4e8ec12a82574593680172ac0418e
michaelmoju <michaelmoju@gmail.com>
2020-06-01 13:44:14 +0800

    convert annotated file to se file

---
+++ ser_annotate/create_se.py
@@ -0,0 +1,33 @@
+from .std import *
+from .dataset_reader.io_cano import CrpCano
+from .dataset_reader.io_SSQA import str_lesson2st
+
+def annot2se(annot_fp):
+	'''
+	Convert annotated file to supporting evidence file.
+	Use keyword positions to determine which sentences are supporting sentences.
+	'''
+	def has_keyword(s, labels):
+		for label in labels:
+			assert label[2] == 'Keyword'
+			l_start = label[0]
+			l_end = label[1]
+			if s[0] <= l_start <= s[1]:
+				assert s[0] <= l_end <= s[1], 'keyword cross sentence!!'
+				return True
+		return False
+	
+	annots = jseq_load(annot_fp)
+	myCrpCano = CrpCano()
+	myCrpCano.load_annot_ssqa(annots)
+	out_q_list = []
+	for qid, q_annot in myCrpCano.items():
+		se = []
+		st_parags = str_lesson2st(q_annot.c_text)
+		for p_i, p in enumerate(st_parags):
+			for s_i, s in enumerate(p[2]):
+				if has_keyword(s, q_annot.labels):
+					se.append([p_i, s_i])
+		#         assert se, "No supporting evidence in qid:{}".format(qid)
+		out_q_list.append({'qid': qid, 'qtext': q_annot.qtext, 'context': st_parags, 'se': se})
+	return out_q_list
